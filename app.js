1e371371d486bae646cce1e655f2f341399ec7b3
Jeniffer Carvalho <jeniffer@agenciapixels.com>
2019-03-15 22:25:16 -0300

    adicionado objetos Storage UI e Weather

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-// Init weather
-const weather = new Weather('Itu', 'br');
+// Init storage
+const storage = new Storage();
 
-weather.getWeather()
-.then( results => {
-    console.log(results);
-})
-.catch(error => console.log(error));
+// get stored location data
+const weatherLocation = storage.getLocationData();
+
+// Init weather
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
+
+//Init UI
+const ui = new UI();
+
+
+// Get weather on dom load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+//change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const country = document.getElementById('country').value;
+
+    //chamge location
+    weather.changeLocation(city, country);
+
+    //set location
+    storage.setLocationData(city, country);
+
+    // get and display weather
+    getWeather();
+
+    // close modal
+    $('#locModal').modal('hide');
+
+});
+
+
+
+function getWeather() {
+    weather.getWeather()
+    .then( results => {
+        ui.paint(results);
+    })
+    .catch(error => console.log(error));
+}
